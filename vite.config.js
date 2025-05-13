cdd2c33f19c5828f8aa0733e9ca900640c7599fb
Giacomo <giacomo.folli@studenti.unipr.it>
2025-05-13 17:30:01 +0200

    chore: simplify Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,25 +2,5 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    plugins: [sveltekit()],
-    server: {
-        host: "0.0.0.0",
-        port: 5000,
-        strictPort: true,
-        hmr: {
-            clientPort: 443,
-        },
-        cors: {
-            origin: "*"
-        },
-        fs: {
-            allow: [".."]
-        },
-        // Allow any host to connect to this server
-        allowedHosts: ["*", ".replit.dev", ".repl.co"]
-    },
-    preview: {
-        host: "0.0.0.0",
-        port: 5000,
-    },
+  plugins: [sveltekit()],
 });
