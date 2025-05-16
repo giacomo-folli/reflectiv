6d32c7647a737c4130732903a2c3d1cbf63d0a8d
Giacomo <giacomo.folli@studenti.unipr.it>
2025-05-16 10:35:26 +0200

    chore: Remove cached files from git

--- src/lib/components/Footer.svelte
+++
@@ -1,7 +0,0 @@
-<footer class="bg-gray-800 py-6 px-6 mt-auto">
-  <div class="container mx-auto">
-    <div class="text-center text-gray-400 text-sm">
-      <p>© {new Date().getFullYear()} Reflection Diary. All rights reserved.</p>
-    </div>
-  </div>
-</footer>
--- src/routes/+page.server.ts
+++
@@ -1,5 +0,0 @@
-import type { PageServerLoad } from "./$types";
-
-export const load = (({ locals }) => {
-  return { user: locals?.user, session: locals.sessionId };
-}) as PageServerLoad;
